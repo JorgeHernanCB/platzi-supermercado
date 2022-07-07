b99175602b9cbe91d946d12c18b4feacf6ced2bd
JorgeHernanCB <jorge.hernantk@gmail.com>
2022-07-07 10:19:58 -0500

    Implementación de los metodos SAVE y DELETE de la entidad Producto

--- src/main/java/com/platzi/supermercado/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/supermercado/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.platzi.supermercado.persistence;
 
 import com.platzi.supermercado.persistence.crud.ProductoCrudRepository;
 import com.platzi.supermercado.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository crudRepository;
@@ -16,4 +19,17 @@ public class ProductoRepository {
     public List<Producto> getByCategoria(int idCategoria){
         return crudRepository.findByIdCategoria(idCategoria);
     }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return crudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return crudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        crudRepository.deleteById(idProducto);
+    }
+
 }
