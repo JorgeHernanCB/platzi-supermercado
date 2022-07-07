597c9b3a8dc593382f6c6e454523f75668dea583
JorgeHernanCB <jorge.hernantk@gmail.com>
2022-07-07 10:08:04 -0500

    Creación de Query Metodos y consultas nativas en ProductoCrudRepository

--- src/main/java/com/platzi/supermercado/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/supermercado/persistence/ProductoRepository.java
@@ -12,4 +12,8 @@ public class ProductoRepository {
     public List<Producto> getAll(){
         return (List<Producto>) crudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return crudRepository.findByIdCategoria(idCategoria);
+    }
 }
--- src/main/java/com/platzi/supermercado/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/supermercado/persistence/crud/ProductoCrudRepository.java
@@ -1,9 +1,16 @@
 package com.platzi.supermercado.persistence.crud;
 
 import com.platzi.supermercado.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
 
+    List<Producto> findByIdCategoria(int idCategoria);
+
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getByCategoria(int idCategoria);
 
 }
